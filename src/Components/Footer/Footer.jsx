f95601d5f9a6e484cca3f532a3786cd72f998fb7
Guada <guadapdls@gmail.com>
2024-11-25 18:01:52 -0300

    updated projects, images, CVs, etc

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
           <a href="https://www.linkedin.com/in/guadalupepiva/" target="_blank"><AiOutlineLinkedin/></a>
         </li>
         <li>
-          <a href="mailto:guadapdls@gmail.com" target="_blank"><AiOutlineMail/></a>
+          <a href="mailto:gupiv.dev@gmail.com" target="_blank"><AiOutlineMail/></a>
         </li>
         <li>
           <a href="https://wa.me/+59894334562" target="_blank"><AiOutlineWhatsApp/></a>
--- src/Components/Sections/Projects/projects.js
+++ src/Components/Sections/Projects/projects.js
@@ -1,6 +1,28 @@
 const projects = [
 	{
 		id: 1,
+		title: "Kingdom Institute | App",
+		description: {
+			es: "Sitio web para Kingdom Institute creado con React JS y SASS, utilizando Bootstrap y AOS. Incluye un test de nivel de inglés y un formulario para enviar emails.",
+			en: "Website for Kingdom Institute created with React JS and SASS, using Bootstrap and AOS. Includes an English level test and a form to send emails.",
+		},
+		img: "../../images/KingdomInstitute.jpg",
+		deploy: "https://kingdom-institute-v2.vercel.app/",
+		source: "https://github.com/Guada-pdls/Kingdom-Institute-v2",
+	},
+	{
+		id: 2,
+		title: "DecorateMe | Backend e-commerce",
+		description: {
+			es: "E-commerce realizado con Express, con base de datos en MongoDB, además de librerías tales como jsonwebtoken, mongoose, nodemailer y express-session. Incluye autenticacion, CRUD de productos, de carrito y de usuarios.",
+			en: "E-commerce made with Express, with database in MongoDB , besides of libraries such as jsonwebtoken, mongoose, nodemailer and express-session. Includes authentication, product, cart and user CRUDs."
+		},
+		img: "../../images/DecorateMe.png",
+		deploy: "https://github.com/Guada-pdls/DecorateMe-Back",
+		source: "https://github.com/Guada-pdls/DecorateMe-Back",
+	},
+	{
+		id: 3,
 		title: "Cattitude | E-commerce",
 		description: {
 			es: "E-commerce de productos para gatitos, desarrollado con React JS y Firestore. También utilicé dependencias como react-icons, react-router-dom, toastify-js, entre otras. Su principal funcionalidad es el carrito.",
@@ -12,37 +34,26 @@ const projects = [
 		source: "https://github.com/Guada-pdls/Cattitude",
 	},
 	{
-		id: 2,
-		title: "Kingdom Institute | App",
-		description: {
-			es: "Sitio web para Kingdom Institute creado con React JS y SASS, utilizando Bootstrap y AOS. Incluye un test de nivel de inglés y un formulario para enviar emails.",
-			en: "Website for Kingdom Institute created with React JS and SASS, using Bootstrap and AOS. Includes an English level test and a form to send emails.",
-		},
-		img: "../../images/KingdomInstitute.jpg",
-		deploy: "https://kingdom-institute-v2.vercel.app/",
-		source: "https://github.com/Guada-pdls/Kingdom-Institute-v2",
-	},
-	{
-		id: 3,
-		title: "To Do | App",
-		description: {
-			es: "App de tareas desarrollada con React JS y Tailwind CSS.",
-			en: "Tasks app developed with React JS and Tailwind CSS.",
-		},
-		img: "../../images/ToDoApp.png",
-		deploy: "https://todo-app-nine-self.vercel.app",
-		source: "https://github.com/Guada-pdls/ToDo-app",
-	},
-	{
 		id: 4,
-		title: "DecorateMe | Backend e-commerce",
+		title: "Veterinaria | App",
 		description: {
-			es: "E-commerce realizado con Express, con base de datos en MongoDB, además de librerías tales como jsonwebtoken, mongoose, nodemailer y express-session. Incluye autenticacion, CRUD de productos, de carrito y de usuarios.",
-			en: "E-commerce made with Express, with database in MongoDB , besides of libraries such as jsonwebtoken, mongoose, nodemailer and express-session. Includes authentication, product, cart and user CRUDs."
+			es: "Aplicación desarrollada en Java con conexion a una base de datos en SQL. Permite realizar un CRUD tanto a los socios como a sus mascotas.",
+			en: "Application developed in Java with connection to a SQL database. Allows CRUD operations on both veterinary partners and their pets."
 		},
-		img: "../../images/FakeStore.jpg",
-		deploy: "https://guada-pdls.github.io/Proyecto-JS/",
-		source: "https://github.com/Guada-pdls/Proyecto-JS",
+		img: "../../images/Veterinaria.png",
+        deploy: "https://github.com/Guada-pdls/Veterinaria",
+        source: "https://github.com/Guada-pdls/Veterinaria",
+	},
+	{
+		id: 5,
+		title: "Rick And Morty | Website",
+		description: {
+			es: "Sitio web que contiene información sobre los personajes, lugares y episodios de la serie Rick And Morty. Hecha con HTML, CSS y Javascript, trae los datos de la Rick And Morty API.",
+			en: "Website that contains information about the characters, locations, and episodes of the Rick And Morty series. Made with HTML, CSS, and JavaScript, it fetches data from the Rick And Morty API."
+		},
+		img: "../../images/RickAndMorty.png",
+		deploy: "https://guada-pdls.github.io/RickAndMorty/",
+		source: "https://github.com/Guada-pdls/RickAndMorty",
 	},
 ];
 
